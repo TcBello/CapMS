6b7aaea0d366be98a5edeef603eac8f8a88695f2
TcBello <78838480+TcBello@users.noreply.github.com>
2022-10-27 11:35:49 +0800

    fix bug on login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,30 +1,65 @@
-import { IonButton, IonCard, IonIcon, IonInput, IonItem, IonLabel, IonPage} from "@ionic/react";
-import { lockClosed } from "ionicons/icons";
-import { Component } from "react";
+import { IonButton, IonCard, IonContent, IonIcon, IonInput, IonItem, IonLabel, IonPage} from "@ionic/react";
+import { lockClosed, mail } from "ionicons/icons";
+import { Component, useState } from "react";
 import BackgroundLogin from "./components/BackgroundLogin";
 import "../../core/components/Spacer.css";
-import MediaQuery from "react-responsive";
-import LoginWeb from "./components/Login-Web";
-import LoginMobile from "./components/Login-Mobile";
+import { useMediaQuery } from "react-responsive";
 import "./Login.css";
+import { webWidth } from "../../core/Utils";
 
-class Login extends Component{
-    constructor(props: {}){
-        super(props);
-    }
+const Login = () => {
+    const isDesktop = useMediaQuery({minWidth: webWidth});
 
-    render(){
-        return (
-            <div>
-                <IonPage>
-                    {/* WEB VERSION */}
-                    <LoginWeb />
-                    {/* MOBILE VERSION */}
-                    <LoginMobile />
-                </IonPage>
-            </div>
-        );
-    }
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return <IonPage>
+        {/* CONTENT */}
+        <IonContent scrollY={false}>
+            {/* BACKGROUND IMAGE */}
+            <BackgroundLogin />
+                <div className="content-center">
+                    <div className="spacer-h-xl"/>
+                    {/* CAPMS HEADER */}
+                    <h1 className="cap-text">Cap<span>MS</span></h1>
+                    <div className="spacer-h-xl"/>
+                    {/* CARD */}
+                    <IonCard className={isDesktop ? "login-card" : "login-card-mobile"}>
+                        {/* LOGIN HEADER */}
+                        <h1 className="login-header">Login</h1>
+                        <div className="spacer-h-l"/>
+                        <div className="input-field-container">
+                            {/* EMAIL ICON */}
+                            <IonIcon icon={mail} className="icon"></IonIcon>
+                            <div className="spacer-w-xs" />
+                            {/* EMAIL INPUT FIELD */}
+                            <IonItem lines="none" className={isDesktop ? "input-field" : "input-field-mobile"}>
+                                <IonLabel position="floating">
+                                    Email
+                                </IonLabel>
+                                <IonInput value={email} onIonChange={(e: any) => setEmail(e.target.value)} />
+                            </IonItem>
+                        </div>
+                        <div className="spacer-h-s"/>
+                        <div className="input-field-container">
+                            {/* PASSWORD ICON */}
+                            <IonIcon icon={lockClosed} className="icon"></IonIcon>
+                            <div className="spacer-w-xs" />
+                            {/* PASSWORD INPUT FIELD */}
+                            <IonItem lines="none" className={isDesktop ? "input-field" : "input-field-mobile"}>
+                                <IonLabel position="floating">
+                                    Password
+                                </IonLabel>
+                                <IonInput type="password" value={password} onIonChange={(e: any) => setPassword(e.target.value)}/>
+                            </IonItem>
+                        </div>
+                        <div className="spacer-h-m"/>
+                        {/* LOGIN BUTTON */}
+                        <IonButton className="login-button" onClick={Login}>LOGIN</IonButton>
+                    </IonCard>
+                </div>
+        </IonContent>
+    </IonPage>
+};
 
 export default Login;
