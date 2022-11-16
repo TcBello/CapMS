fa065ea0f84ddc8d1a2f102a6f5135151a6abdb3
Ericka <erickajane.macatol@g.batstate-u.edu.ph>
2022-11-16 14:29:06 +0800

    as
    sas

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -12,18 +12,104 @@ import { MobileMenuAppBar } from "../../core/components/Mobile-Appbar";
 import UserModel, { setUserModel } from "../../core/models/user_model";
 import { getAdminProfile } from "../../core/services/admin_service";
 
-class Profile extends Component {
-    render() {
-        return (
-            <IonPage>
-                {/* PROFILE WEB VERSION */}
-                <ProfileWeb />
-                {/* PROFILE MOBILE VERSION */}
-                <ProfileMobile />
-            </IonPage>
-        );
-    }
-}
+const Profile = () => {
+    const isDesktop = useMediaQuery({ minWidth: webWidth });
+
+    const [user, setUser] = useState<UserModel>(setUserModel({}));
+
+    const userData = getStorageData("user");
+    const userModel = (JSON.parse(userData!)) as UserModel;
+
+    useEffect(() => {
+        getAdminProfile(userModel.uid).then((value: any) => {
+            setUser(value as UserModel);
+        });
+    }, []);
+
+    return (
+        <IonPage>
+            {/* APP BAR */}
+            {
+                isDesktop
+                    ? <ContentHeader title="User Profile" />
+                    : <MobileMenuAppBar title="User Profile" />
+            }
+            <IonContent className={isDesktop ? "profile-content" : "profile-content-mpbile"}>
+                {
+                    isDesktop
+                    // DESKTOP VIEW
+                        ? <div className="profile-container">
+                            <div className="colorHeader" />
+                            <div className="content-left">
+                                {/* INFORMATIONS */}
+                                <IonList>
+                                    {/* NAME */}
+                                    <IonItem lines="none">
+                                        <IonIcon icon={person} slot="start" color="black"></IonIcon>
+                                        <IonLabel>{userModel.firstName +" "+ userModel.lastName}</IonLabel>
+                                    </IonItem>
+                                    {/* EMAIL */}
+                                    <IonItem lines="none">
+                                        <IonIcon icon={mail} slot="start" color="black"></IonIcon>
+                                        <IonLabel>{user.email}</IonLabel>
+                                    </IonItem>
+                                      {/* SR CODE */}
+                                      <IonItem lines="none">
+                                        <IonIcon src="/assets/icon/srcode-logo.svg" slot="start" color="black"></IonIcon>
+                                        <IonLabel>{userModel.srCode}</IonLabel>
+                                    </IonItem>
+                                    {/* MAJOR IN */}
+                                    <IonItem lines="none">
+                                        <IonIcon icon={school} slot="start" color="black"></IonIcon>
+                                        <IonLabel>{userModel.course}</IonLabel>
+                                    </IonItem>
+                                </IonList>
+                                <div className="content-right">
+                                    {/* PROFILE IMAGE */}
+                                    <IonAvatar className="profile-image">
+                                        <img src={defaultImage} />
+                                    </IonAvatar>
+                                    {/* EDIT BUTTON */}
+                                    <IonButton className="edit-button" href="/home/admin/profile/edit-password">Edit Password</IonButton>
+                                </div>
+                            </div>
+                        </div>
+                        // MOBILE VIEW
+                        : <div className="profile-mobile-container">
+                            <div className="profile-mobile-content-center">
+                            <div className="spacer-h-l"/>
+                                {/* PHOTO */}
+                                <IonAvatar className="photo">
+                                    <img src={defaultImage} />
+                                </IonAvatar>
+                                 {/* NAME */}
+                                 <h5 className="name">{userModel.firstName + " " + userModel.lastName}</h5>
+                                {/* ROLE */}
+                                <h5 className="role">{user.role}</h5>
+                            </div>
+                            <div className="spacer-h-m"/>
+                            {/* EMAIL */}
+                            <IonItem lines="none">
+                                <IonIcon icon={mail} slot="start" />
+                                <IonLabel>{user.email}</IonLabel>
+                            </IonItem>
+                                    {/* SR CODE */}
+                                    <IonItem lines="none">
+                                        <IonIcon src="/assets/icon/srcode-logo.svg" slot="start" color="black"></IonIcon>
+                                        <IonLabel>{userModel.srCode}</IonLabel>
+                                    </IonItem>
+                                    {/* MAJOR IN */}
+                                    <IonItem lines="none">
+                                        <IonIcon icon={school} slot="start" color="black"></IonIcon>
+                                        <IonLabel>{userModel.course}</IonLabel>
+                                    </IonItem>
+                            <IonButton className="edit-button-mobile" shape="round" href="/home/admin/profile/edit-password">Edit Password</IonButton>
+                        </div>
+                }
+            </IonContent>
+        </IonPage>
+    );
+};
 
 const ProfileAdmin = () => {
     const isDesktop = useMediaQuery({ minWidth: webWidth });
@@ -104,5 +190,4 @@ const ProfileAdmin = () => {
     );
 };
 
-export { ProfileAdmin };
-export default Profile;
+export { ProfileAdmin, Profile };
